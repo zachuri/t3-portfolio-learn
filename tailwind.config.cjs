ca79ab9f7bf69a7be3fc7974f7c44b3702b68989
zachuri <punsalang.zachary@gmail.com>
2022-09-07 20:14:16 -0700

    setup: tailwind.css config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+	content: ["./src/**/*.{js,ts,jsx,tsx}"],
+	theme: {
+		letterSpacing: {
+			tight: "-.015em",
+		},
+		extend: {
+			height: {
+				'half': '50vh',
+			},
+		},
+	},
+	plugins: [],
 };
