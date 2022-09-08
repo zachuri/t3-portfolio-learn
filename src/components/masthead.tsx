546946ddb13323cc303c0504c53b64f14eb8305f
zachuri <punsalang.zachary@gmail.com>
2022-09-07 23:13:22 -0700

    masthead: changed name to uppercase

--- src/components/masthead.tsx
+++ src/components/masthead.tsx
@@ -20,7 +20,7 @@ const Masthead: React.FC = () => {
 				<Image src="/favicon.ico" width={128 / 3} height={114 / 3} />
 			</div>
 			<div className="p-12 font-bold z-10 text-white drop-shadow-[0_5px_3px_rgba(0,0,0,0.4)] text-center flex-1 flex items-center justify-center flex-col">
-				<h1 className="mb-6 text-4xl xl:text-5xl">Zachuri</h1>
+				<h1 className="mb-6 text-4xl xl:text-5xl uppercase">zachuri</h1>
 				<h2 className="mb-2 text-2xl xl:text-3xl tracking-tight">
 					<span>
 						Full <span>Stack</span> Developer
