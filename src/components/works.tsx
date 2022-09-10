eb1358ba140e3fb5938b3d6ec65b20ed0a414c47
zachuri <punsalang.zachary@gmail.com>
2022-09-09 18:23:37 -0700

    parallaxEffect: start on right side:

--- src/components/works.tsx
+++ src/components/works.tsx
@@ -1,7 +1,7 @@
 import { spawn } from "child_process";
 import React from "react";
 import { Tile, TileBackground, TileWrapper, TileContent } from "./tile";
-import { WorkBackground, WorkContainer, WorkLeft } from "./work";
+import { WorkBackground, WorkContainer, WorkLeft, WorkRight } from "./work";
 
 const Works = () => {
 	return (
@@ -16,8 +16,14 @@ const Works = () => {
 						<WorkContainer>
 							<WorkLeft progress={progress}>
 								<div>We built</div>
+								<div className="text-4xl md:text-5xl font-semibold tracking-tight">
+									PinkPanda We've built
+								</div>
 							</WorkLeft>
 							{/* <span className="text-9xl">Foo {progress}</span> */}
+							<WorkRight progress={progress}>
+								<div>We built</div>
+							</WorkRight>
 						</WorkContainer>
 					)}
 				></Tile>
