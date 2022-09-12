755641480c00c054aeb196a1d568b3a6d085fb77
zachuri <punsalang.zachary@gmail.com>
2022-09-11 22:24:28 -0700

    added: sizeobserver in my _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,15 @@
 import "../styles/globals.css";
 import type { AppType } from "next/dist/shared/lib/utils";
 import ScrollObservor from "../utils/scroll-observer";
+import SizeObserver from "../utils/size-observer";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
 	return (
-		<ScrollObservor>
-			<Component {...pageProps} />
-		</ScrollObservor>
+		<SizeObserver>
+			<ScrollObservor>
+				<Component {...pageProps} />
+			</ScrollObservor>
+		</SizeObserver>
 	);
 };
 
