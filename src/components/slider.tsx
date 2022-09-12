3b0602278a42553f46b910eab74d68ba987800bb
zachuri <punsalang.zachary@gmail.com>
2022-09-11 23:11:08 -0700

    slider: created a slider component

---
+++ src/components/slider.tsx
@@ -0,0 +1,37 @@
+import React, { useContext, useRef } from "react";
+import { SizeContext } from "../utils/size-observer";
+
+interface Props {
+	initialOffsetX: number;
+	className: string;
+	contentWidth: number;
+}
+
+const SliderContainer: React.FC<Props> = ({
+	children,
+	initialOffsetX,
+	className,
+	contentWidth,
+}) => {
+	const { innerWidth } = useContext(SizeContext);
+
+	const refScrollX = useRef<number>(initialOffsetX);
+	const refContainer = useRef<HTMLDivElement>(null);
+	const refContent = useRef<HTMLDivElement>(null);
+
+	const enabled = innerWidth < contentWidth;
+
+	return (
+		<div ref={refContainer} className={`"slider-container overflow-x-hidden whitespace-nowrap max-w-full pointer-events-none ${className}"`}>
+			<div ref={refContainer} className="inline-block">
+				{children}
+			</div>
+		</div>
+	);
+};
+
+const Slider = () => {
+	return <div>slider</div>;
+};
+
+export default Slider;
