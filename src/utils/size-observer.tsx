d4e07507122570613b4656cec7147a89306dda91
zachuri <punsalang.zachary@gmail.com>
2022-09-14 01:16:01 -0700

    size-observer: added children type

--- src/utils/size-observer.tsx
+++ src/utils/size-observer.tsx
@@ -1,30 +1,30 @@
 import React, { useCallback, useEffect, useState } from "react";
 
 interface ScrollValue {
-	innerWidth: number;
+  innerWidth: number;
 }
 
 export const SizeContext = React.createContext<ScrollValue>({
-	innerWidth: 0,
+  innerWidth: 0,
 });
 
-const SizeObserver: React.FC = ({ children }) => {
-	const [innerWidth, setInnerWidth] = useState(0);
-	const handelResize = useCallback(() => {
-		setInnerWidth(window.innerWidth);
-	}, []);
+const SizeObserver: React.FC<{ children?: React.ReactNode }> = ({ children }) => {
+  const [innerWidth, setInnerWidth] = useState(0);
+  const handelResize = useCallback(() => {
+    setInnerWidth(window.innerWidth);
+  }, []);
 
-	useEffect(() => {
-		handelResize();
-		window.addEventListener("resize", handelResize, { passive: true });
-		return () => window.removeEventListener("resize", handelResize);
-	}, [handelResize]);
+  useEffect(() => {
+    handelResize();
+    window.addEventListener("resize", handelResize, { passive: true });
+    return () => window.removeEventListener("resize", handelResize);
+  }, [handelResize]);
 
-	return (
-		<SizeContext.Provider value={{ innerWidth }}>
-			{children}
-		</SizeContext.Provider>
-	);
+  return (
+    <SizeContext.Provider value={{ innerWidth }}>
+      {children}
+    </SizeContext.Provider>
+  );
 };
 
 export default SizeObserver;
