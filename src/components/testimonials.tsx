58d302b7380e2844e05b250c697e752514079dd9
zachuri <punsalang.zachary@gmail.com>
2022-09-13 21:08:01 -0700

    testimonials: check multiple reviews (works)

--- src/components/testimonials.tsx
+++ src/components/testimonials.tsx
@@ -1,12 +1,22 @@
 import React from "react";
 import Carousel from "./carousel";
 import CarouselItem from "./carousel-item";
+import Review from "./review";
 
 const Testimonials: React.FC = () => {
 	return (
 		<Carousel className="bg-black text-white py-10 lg:py-20">
 			<CarouselItem index={0}>
-				<div>Review 1</div>
+				<Review by="Axel (Showtime)">
+					Margelo and Showtime both share the love for high-quality
+					software and the passion for building something people want.
+				</Review>
+			</CarouselItem>
+			<CarouselItem index={1}>
+				<Review by="Axel (Showtime)">
+					Margelo and Showtime both share the love for high-quality
+					software and the passion for building something people want.
+				</Review>
 			</CarouselItem>
 		</Carousel>
 	);
